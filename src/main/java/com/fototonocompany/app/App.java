61f555dfc6bca12cb3fc27154d4de35e4380b20c
jesus <jesusmoralesaguiar@gmail.com>
2016-09-04 17:43:25 +0200

    mvn example

---
+++ src/main/java/com/fototonocompany/app/App.java
@@ -0,0 +1,13 @@
+package com.fototonocompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
